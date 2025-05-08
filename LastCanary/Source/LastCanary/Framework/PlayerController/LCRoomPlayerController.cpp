49672e999cfd9643265ae4294278b1dc463f2477
dlsgjs100 <inkskku@gmail.com>
2025-05-08 14:49:20 +0900

    [Add] RoomFramework

---
+++ LastCanary/Source/LastCanary/Framework/PlayerController/LCRoomPlayerController.cpp
@@ -0,0 +1,2 @@
+#include "Framework/PlayerController/LCRoomPlayerController.h"
+
---
+++ LastCanary/Source/LastCanary/Framework/PlayerController/LCRoomPlayerController.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "LCRoomPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LASTCANARY_API ALCRoomPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
