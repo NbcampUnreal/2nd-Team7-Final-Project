121e0e4134f4e7e361c831e0bc56da428cb558d0
Mrsangmin <sangmin0280@naver.com>
2025-05-15 15:17:47 +0900

    [Add] ResultMenu, RewardEntry 추가

---
+++ LastCanary/Source/LastCanary/UI/UIObject/RewardEntry.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/LCUserWidgetBase.h"
+#include "RewardEntry.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LASTCANARY_API URewardEntry : public ULCUserWidgetBase
+{
+	GENERATED_BODY()
+	
+};
