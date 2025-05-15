bd75ae35adafb447e110273e6a829b53d2dad274
ljh3041 <ljh3041@hanmail.net>
2025-05-15 16:56:47 +0900

    [fix] Mantling 오류 수정 겸 Mantling 기능 비활성화
    Mantling 오류 수정 겸 Mantling 기능 비활성화

--- LastCanary/Source/LastCanary/Character/BaseCharacter.cpp
+++ LastCanary/Source/LastCanary/Character/BaseCharacter.cpp
@@ -134,11 +134,6 @@ void ABaseCharacter::Handle_Jump(const FInputActionValue& ActionValue)
 			return;
 		}
 
-		if (StartMantlingGrounded())
-		{
-			return;
-		}
-
 		if (GetStance() == AlsStanceTags::Crouching)
 		{
 			SetDesiredStance(AlsStanceTags::Standing);
