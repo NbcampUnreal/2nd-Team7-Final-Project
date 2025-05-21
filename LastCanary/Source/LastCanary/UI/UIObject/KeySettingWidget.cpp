6265c331935c0267ca7d49e78ea4742571c91586
dlsgjs100 <inkskku@gmail.com>
2025-05-21 17:07:02 +0900

    [Update] 옵션에 키세팅 추가

--- LastCanary/Source/LastCanary/UI/UIObject/KeySettingWidget.cpp
+++ LastCanary/Source/LastCanary/UI/UIObject/KeySettingWidget.cpp
@@ -1,73 +1,119 @@
 #include "KeySettingWidget.h"
+
 #include "Components/InputKeySelector.h"
+#include "Components/TextBlock.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "InputModifiers.h"
 
 void UKeySettingWidget::NativeConstruct()
 {
     Super::NativeConstruct();
 
-    if (InputKeySelectorFrontKeyboard)
-    {
-        InputKeySelectorFrontKeyboard->OnKeySelected.AddUniqueDynamic(this, &UKeySettingWidget::OnKeySelectedFront);
-    }
-    if (InputKeySelectorBackKeyboard)
-    {
-        InputKeySelectorBackKeyboard->OnKeySelected.AddUniqueDynamic(this, &UKeySettingWidget::OnKeySelectedBack);
-    }
-    if (InputKeySelectorLeftKeyboard)
-    {
-        InputKeySelectorLeftKeyboard->OnKeySelected.AddUniqueDynamic(this, &UKeySettingWidget::OnKeySelectedLeft);
-    }
-    if (InputKeySelectorRightKeyboard)
-    {
-        InputKeySelectorRightKeyboard->OnKeySelected.AddUniqueDynamic(this, &UKeySettingWidget::OnKeySelectedRight);
-    }
-    if (InputKeySelectorJumpKeyboard)
-    {
-        InputKeySelectorJumpKeyboard->OnKeySelected.AddUniqueDynamic(this, &UKeySettingWidget::OnKeySelectedJump);
-    }
-    if (InputKeySelectorCrouchKeyboard)
-    {
-        InputKeySelectorCrouchKeyboard->OnKeySelected.AddUniqueDynamic(this, &UKeySettingWidget::OnKeySelectedCrouch);
-    }
-    if (InputKeySelectorWalkKeyboard)
-    {
-        InputKeySelectorWalkKeyboard->OnKeySelected.AddUniqueDynamic(this, &UKeySettingWidget::OnKeySelectedWalk);
-    }
-    if (InputKeySelectorRollKeyboard)
-    {
-        InputKeySelectorRollKeyboard->OnKeySelected.AddUniqueDynamic(this, &UKeySettingWidget::OnKeySelectedRoll);
-    }
+    InitRow(TEXT("Move Forward"), IA_MoveForward, ActionLabelMoveForward, CurrentKeyMoveForward, SelectorMoveForward);
+    InitRow(TEXT("Move Backward"), IA_MoveBackward, ActionLabelMoveBackward, CurrentKeyMoveBackward, SelectorMoveBackward);
+    InitRow(TEXT("Strafe Left"), IA_StrafeLeft, ActionLabelStrafeLeft, CurrentKeyStrafeLeft, SelectorStrafeLeft);
+    InitRow(TEXT("Strafe Right"), IA_StrafeRight, ActionLabelStrafeRight, CurrentKeyStrafeRight, SelectorStrafeRight);
+    InitRow(TEXT("Walk"), IA_Walk, ActionLabelWalk, CurrentKeyWalk, SelectorWalk);
+    InitRow(TEXT("Sprint"), IA_Sprint, ActionLabelSprint, CurrentKeySprint, SelectorSprint);
+    InitRow(TEXT("Jump"), IA_Jump, ActionLabelJump, CurrentKeyJump, SelectorJump);
+    InitRow(TEXT("Crouch"), IA_Crouch, ActionLabelCrouch, CurrentKeyCrouch, SelectorCrouch);
+    InitRow(TEXT("Interact"), IA_Interact, ActionLabelInteract, CurrentKeyInteract, SelectorInteract);
+
+    SelectorMoveForward->OnKeySelected.AddDynamic(this, &UKeySettingWidget::OnKeySelectedMoveForward);
+    SelectorMoveBackward->OnKeySelected.AddDynamic(this, &UKeySettingWidget::OnKeySelectedMoveBackward);
+    SelectorStrafeLeft->OnKeySelected.AddDynamic(this, &UKeySettingWidget::OnKeySelectedStrafeLeft);
+    SelectorStrafeRight->OnKeySelected.AddDynamic(this, &UKeySettingWidget::OnKeySelectedStrafeRight);
+    SelectorWalk->OnKeySelected.AddDynamic(this, &UKeySettingWidget::OnKeySelectedWalk);
+    SelectorSprint->OnKeySelected.AddDynamic(this, &UKeySettingWidget::OnKeySelectedSprint);
+    SelectorJump->OnKeySelected.AddDynamic(this, &UKeySettingWidget::OnKeySelectedJump);
+    SelectorCrouch->OnKeySelected.AddDynamic(this, &UKeySettingWidget::OnKeySelectedCrouch);
+    SelectorInteract->OnKeySelected.AddDynamic(this, &UKeySettingWidget::OnKeySelectedInteract);
 }
 
-void UKeySettingWidget::OnKeySelectedFront(FInputChord SelectedKey)
+void UKeySettingWidget::InitRow(const FString& DisplayName, UInputAction* Action, UTextBlock* Label, UTextBlock* KeyText, UInputKeySelector* Selector)
 {
-    UE_LOG(LogTemp, Log, TEXT("[KeySetting] Front : %s"), *SelectedKey.Key.ToString());
+    if (!Action || !Label || !KeyText || !Selector || !MappingContext)
+    {
+        return;
+    }
+
+    Label->SetText(FText::FromString(DisplayName));
+    FKey CurrentKey = GetMappedKey(Action);
+    KeyText->SetText(FText::FromName(CurrentKey.GetFName()));
+    Selector->SetSelectedKey(CurrentKey);
 }
-void UKeySettingWidget::OnKeySelectedBack(FInputChord SelectedKey)
+
+FKey UKeySettingWidget::GetMappedKey(UInputAction* Action) const
 {
-    UE_LOG(LogTemp, Log, TEXT("[KeySetting] Back : %s"), *SelectedKey.Key.ToString());
+    if (!Action || !MappingContext)
+    {
+        return EKeys::Invalid;
+    }
+
+    for (const FEnhancedActionKeyMapping& Mapping : MappingContext->GetMappings())
+    {
+        if (Mapping.Action == Action)
+        {
+            return Mapping.Key;
+        }
+    }
+    return EKeys::Invalid;
 }
-void UKeySettingWidget::OnKeySelectedLeft(FInputChord SelectedKey)
+
+void UKeySettingWidget::SetMappedKey(UInputAction* Action, FKey NewKey)
 {
-    UE_LOG(LogTemp, Log, TEXT("[KeySetting] Left : %s"), *SelectedKey.Key.ToString());
+    if (!Action || !MappingContext)
+    {
+        return;
+    }
+
+    MappingContext->UnmapKey(Action, GetMappedKey(Action));
+    MappingContext->MapKey(Action, NewKey);
 }
-void UKeySettingWidget::OnKeySelectedRight(FInputChord SelectedKey)
+
+void UKeySettingWidget::OnKeySelectedMoveForward(FInputChord SelectedKey)
 {
-    UE_LOG(LogTemp, Log, TEXT("[KeySetting] Right : %s"), *SelectedKey.Key.ToString());
+    SetMappedKey(IA_MoveForward, SelectedKey.Key);
+    CurrentKeyMoveForward->SetText(FText::FromName(SelectedKey.Key.GetFName()));
 }
-void UKeySettingWidget::OnKeySelectedJump(FInputChord SelectedKey)
+void UKeySettingWidget::OnKeySelectedMoveBackward(FInputChord SelectedKey)
 {
-    UE_LOG(LogTemp, Log, TEXT("[KeySetting] Jump : %s"), *SelectedKey.Key.ToString());
+    SetMappedKey(IA_MoveBackward, SelectedKey.Key);
+    CurrentKeyMoveBackward->SetText(FText::FromName(SelectedKey.Key.GetFName()));
 }
-void UKeySettingWidget::OnKeySelectedCrouch(FInputChord SelectedKey)
+void UKeySettingWidget::OnKeySelectedStrafeLeft(FInputChord SelectedKey)
 {
-    UE_LOG(LogTemp, Log, TEXT("[KeySetting] Crouch : %s"), *SelectedKey.Key.ToString());
+    SetMappedKey(IA_StrafeLeft, SelectedKey.Key);
+    CurrentKeyStrafeLeft->SetText(FText::FromName(SelectedKey.Key.GetFName()));
+}
+void UKeySettingWidget::OnKeySelectedStrafeRight(FInputChord SelectedKey)
+{
+    SetMappedKey(IA_StrafeRight, SelectedKey.Key);
+    CurrentKeyStrafeRight->SetText(FText::FromName(SelectedKey.Key.GetFName()));
 }
 void UKeySettingWidget::OnKeySelectedWalk(FInputChord SelectedKey)
 {
-    UE_LOG(LogTemp, Log, TEXT("[KeySetting] Walk : %s"), *SelectedKey.Key.ToString());
+    SetMappedKey(IA_Walk, SelectedKey.Key);
+    CurrentKeyWalk->SetText(FText::FromName(SelectedKey.Key.GetFName()));
 }
-void UKeySettingWidget::OnKeySelectedRoll(FInputChord SelectedKey)
+void UKeySettingWidget::OnKeySelectedSprint(FInputChord SelectedKey)
 {
-    UE_LOG(LogTemp, Log, TEXT("[KeySetting] Roll : %s"), *SelectedKey.Key.ToString());
+    SetMappedKey(IA_Sprint, SelectedKey.Key);
+    CurrentKeySprint->SetText(FText::FromName(SelectedKey.Key.GetFName()));
+}
+void UKeySettingWidget::OnKeySelectedJump(FInputChord SelectedKey)
+{
+    SetMappedKey(IA_Jump, SelectedKey.Key);
+    CurrentKeyJump->SetText(FText::FromName(SelectedKey.Key.GetFName()));
+}
+void UKeySettingWidget::OnKeySelectedCrouch(FInputChord SelectedKey)
+{
+    SetMappedKey(IA_Crouch, SelectedKey.Key);
+    CurrentKeyCrouch->SetText(FText::FromName(SelectedKey.Key.GetFName()));
+}
+void UKeySettingWidget::OnKeySelectedInteract(FInputChord SelectedKey)
+{
+    SetMappedKey(IA_Interact, SelectedKey.Key);
+    CurrentKeyInteract->SetText(FText::FromName(SelectedKey.Key.GetFName()));
 }
--- LastCanary/Source/LastCanary/UI/UIObject/KeySettingWidget.h
+++ LastCanary/Source/LastCanary/UI/UIObject/KeySettingWidget.h
@@ -2,56 +2,121 @@
 
 #include "CoreMinimal.h"
 #include "UI/LCUserWidgetBase.h"
-#include "InputCoreTypes.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
 #include "KeySettingWidget.generated.h"
 
-/**
- *
- */
 class UInputKeySelector;
 class UTextBlock;
+
 UCLASS()
 class LASTCANARY_API UKeySettingWidget : public ULCUserWidgetBase
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
 protected:
-    virtual void NativeConstruct() override;
+	virtual void NativeConstruct() override;
 
-    UPROPERTY(meta = (BindWidget))
-    UInputKeySelector* InputKeySelectorFrontKeyboard;
-    UPROPERTY(meta = (BindWidget))
-    UInputKeySelector* InputKeySelectorBackKeyboard;
-    UPROPERTY(meta = (BindWidget))
-    UInputKeySelector* InputKeySelectorLeftKeyboard;
-    UPROPERTY(meta = (BindWidget))
-    UInputKeySelector* InputKeySelectorRightKeyboard;
-    UPROPERTY(meta = (BindWidget))
-    UInputKeySelector* InputKeySelectorJumpKeyboard;
-    UPROPERTY(meta = (BindWidget))
-    UInputKeySelector* InputKeySelectorCrouchKeyboard;
-    UPROPERTY(meta = (BindWidget))
-    UInputKeySelector* InputKeySelectorWalkKeyboard;
-    UPROPERTY(meta = (BindWidget))
-    UInputKeySelector* InputKeySelectorRollKeyboard;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputMappingContext* MappingContext;
 
-    // TODO : 필요하다면 Gamepad용도 추가
-    // ...
+	// 액션들
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input") UInputAction* IA_MoveForward;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input") UInputAction* IA_MoveBackward;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input") UInputAction* IA_StrafeLeft;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input") UInputAction* IA_StrafeRight;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input") UInputAction* IA_Walk;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input") UInputAction* IA_Sprint;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input") UInputAction* IA_Jump;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input") UInputAction* IA_Crouch;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input") UInputAction* IA_Interact;
 
-    UFUNCTION()
-    void OnKeySelectedFront(FInputChord SelectedKey);
-    UFUNCTION()
-    void OnKeySelectedBack(FInputChord SelectedKey);
-    UFUNCTION()
-    void OnKeySelectedLeft(FInputChord SelectedKey);
-    UFUNCTION()
-    void OnKeySelectedRight(FInputChord SelectedKey);
-    UFUNCTION()
-    void OnKeySelectedJump(FInputChord SelectedKey);
-    UFUNCTION()
-    void OnKeySelectedCrouch(FInputChord SelectedKey);
-    UFUNCTION()
-    void OnKeySelectedWalk(FInputChord SelectedKey);
-    UFUNCTION()
-    void OnKeySelectedRoll(FInputChord SelectedKey);
+	// 바인딩 위젯
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ActionLabelMoveForward;
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* CurrentKeyMoveForward;
+	UPROPERTY(meta = (BindWidget))
+	UInputKeySelector* SelectorMoveForward;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ActionLabelMoveBackward;
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* CurrentKeyMoveBackward;
+	UPROPERTY(meta = (BindWidget))
+	UInputKeySelector* SelectorMoveBackward;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ActionLabelStrafeLeft;
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* CurrentKeyStrafeLeft;
+	UPROPERTY(meta = (BindWidget))
+	UInputKeySelector* SelectorStrafeLeft;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ActionLabelStrafeRight;
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* CurrentKeyStrafeRight;
+	UPROPERTY(meta = (BindWidget))
+	UInputKeySelector* SelectorStrafeRight;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ActionLabelWalk;
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* CurrentKeyWalk;
+	UPROPERTY(meta = (BindWidget))
+	UInputKeySelector* SelectorWalk;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ActionLabelSprint;
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* CurrentKeySprint;
+	UPROPERTY(meta = (BindWidget))
+	UInputKeySelector* SelectorSprint;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ActionLabelJump;
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* CurrentKeyJump;
+	UPROPERTY(meta = (BindWidget))
+	UInputKeySelector* SelectorJump;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ActionLabelCrouch;
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* CurrentKeyCrouch;
+	UPROPERTY(meta = (BindWidget))
+	UInputKeySelector* SelectorCrouch;
+
+	//UPROPERTY(meta = (BindWidget)) 
+	UTextBlock* ActionLabelInteract;
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* CurrentKeyInteract;
+	UPROPERTY(meta = (BindWidget))
+	UInputKeySelector* SelectorInteract;
+
+	// 유틸
+	void InitRow(const FString& DisplayName, UInputAction* Action, UTextBlock* Label, UTextBlock* KeyText, UInputKeySelector* Selector);
+	FKey GetMappedKey(UInputAction* Action) const;
+	void SetMappedKey(UInputAction* Action, FKey NewKey);
+
+	// 바인딩 이벤트 핸들러
+	UFUNCTION()
+	void OnKeySelectedMoveForward(FInputChord SelectedKey);
+	UFUNCTION()
+	void OnKeySelectedMoveBackward(FInputChord SelectedKey);
+	UFUNCTION()
+	void OnKeySelectedStrafeLeft(FInputChord SelectedKey);
+	UFUNCTION()
+	void OnKeySelectedStrafeRight(FInputChord SelectedKey);
+	UFUNCTION()
+	void OnKeySelectedWalk(FInputChord SelectedKey);
+	UFUNCTION()
+	void OnKeySelectedSprint(FInputChord SelectedKey);
+	UFUNCTION()
+	void OnKeySelectedJump(FInputChord SelectedKey);
+	UFUNCTION()
+	void OnKeySelectedCrouch(FInputChord SelectedKey);
+	UFUNCTION()
+	void OnKeySelectedInteract(FInputChord SelectedKey);
 };
