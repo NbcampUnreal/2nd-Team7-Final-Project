bfc5aa7b644bffcc26a1951c07efc89378fe77e4
Han jeong hyeok <gkswjdgur552@gmail.com>
2025-05-21 19:46:01 +0900

    [Add] Host Menu (Server Travel 테스트용 - 수정 필요)

--- LastCanary/Source/LastCanary/Framework/GameMode/LCLobbyGameMode.cpp
+++ LastCanary/Source/LastCanary/Framework/GameMode/LCLobbyGameMode.cpp
@@ -8,3 +8,15 @@ void ALCLobbyGameMode::TryJoinRoom(ALCLobbyPlayerController* RequestingPC, const
 	UE_LOG(LogTemp, Warning, TEXT("TryJoinRoom called with RoomID: %s, Password: %s"), *RoomID, *Password);
 }
 
+void ALCLobbyGameMode::StartGame()
+{
+    UWorld* World = GetWorld();
+    if (World)
+    {
+        FString MapName = TEXT("/Game/_LastCanary/Dev/Server/MainLevel"); // 실제 MainLevel 경로로 바꾸세요
+        FString TravelURL = MapName + TEXT("?listen");
+
+        World->ServerTravel(TravelURL, true);
+    }
+}
+
