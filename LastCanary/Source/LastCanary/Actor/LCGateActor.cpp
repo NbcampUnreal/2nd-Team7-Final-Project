fa8309d4a8e3f404c538c78c394393a7dfceff95
dlsgjs100 <inkskku@gmail.com>
2025-05-26 17:43:30 +0900

    [Add] LCGateActor생성

---
+++ LastCanary/Source/LastCanary/Actor/LCGateActor.cpp
@@ -0,0 +1,52 @@
+#include "Actor/LCGateActor.h"
+#include "Framework/GameInstance/LCGameInstance.h"
+#include "GameFramework/PlayerController.h"
+#include "Framework/GameInstance/LCGameInstanceSubsystem.h"
+
+#include "LastCanary.h"
+
+ALCGateActor::ALCGateActor()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+    TravelType = EGateTravelType::ToInGame;
+}
+
+void ALCGateActor::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void ALCGateActor::Interact_Implementation(APlayerController* Controller)
+{
+	LOG_Frame_WARNING(TEXT("Gate actor %s is being interacted with."), *GetName());
+	if (HasAuthority() ==false)
+	{
+		LOG_Frame_WARNING(TEXT("Gate interaction failed: no authority."));
+		return;
+	}
+	if (Controller==nullptr)
+	{
+		LOG_Frame_WARNING(TEXT("Gate interaction failed: no controller."));
+		return;
+	}
+
+	if (ULCGameInstanceSubsystem* GISubsystem = GetGameInstance()->GetSubsystem<ULCGameInstanceSubsystem>())
+	{
+		if (TargetMapID != 0)
+		{
+			//GISubsystem->ChangeLevelByMapID(TargetMapID);
+			LOG_Frame_WARNING(TEXT("Gate actor %s is changing level to map ID %d."), *GetName(), TargetMapID);
+		}
+		else
+		{
+			LOG_Frame_WARNING(TEXT("No valid target map set on GateActor."));
+		}
+	}
+}
+
+void ALCGateActor::SetTargetMapID(const int32& InMapID)
+{
+	TargetMapID = InMapID;
+}
