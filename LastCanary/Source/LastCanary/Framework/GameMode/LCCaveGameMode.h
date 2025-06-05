90753e9093fc516aab1c25e394b02545fb0d8aca
Han jeong hyeok <gkswjdgur552@gmail.com>
2025-06-05 17:18:07 +0900

    [Add] InGameModeBase & Ruins, Cave GameMode

---
+++ LastCanary/Source/LastCanary/Framework/GameMode/LCCaveGameMode.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Framework/GameMode/LCInGameModeBase.h"
+#include "LCCaveGameMode.generated.h"
+
+UCLASS()
+class LASTCANARY_API ALCCaveGameMode : public ALCInGameModeBase
+{
+	GENERATED_BODY()
+	
+};
