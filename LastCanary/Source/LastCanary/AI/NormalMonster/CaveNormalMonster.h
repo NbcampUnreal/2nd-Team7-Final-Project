9a8d0766ad5d89dcba41272e4f0fc2aa8f4fc8fb
GeonWoo <lgs2940aa@gmail.com>
2025-06-08 15:23:23 +0900

    모든 몬스터 사운드 추가, 불 필요한 사운드 삭제, 건 베이스 사운드 조절, 엘리트 몬스터 구분 (임시, 중간 이후 구분), 주석 삭제 및 수정

---
+++ LastCanary/Source/LastCanary/AI/NormalMonster/CaveNormalMonster.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AI/BaseMonsterCharacter.h"
+#include "CaveNormalMonster.generated.h"
+
+UCLASS()
+class LASTCANARY_API ACaveNormalMonster : public ABaseMonsterCharacter
+{
+	GENERATED_BODY()
+	
+};
