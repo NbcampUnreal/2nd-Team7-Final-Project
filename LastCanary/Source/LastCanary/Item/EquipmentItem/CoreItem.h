ec3b17331f8b30dd14e5e5cf5b6420185da82219
dlsgjs100 <inkskku@gmail.com>
2025-06-18 17:58:05 +0900

    [Add] 코어방 수정 및 상호작용 가능하도록 변경, 코어 아이템 별도 클래스로 구현

---
+++ LastCanary/Source/LastCanary/Item/EquipmentItem/CoreItem.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item/EquipmentItem/EquipmentItemBase.h"
+#include "CoreItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LASTCANARY_API ACoreItem : public AEquipmentItemBase
+{
+	GENERATED_BODY()
+	
+};
