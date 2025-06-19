ae231f5cdf6a2a609c6cc4e2a65a4fbb10fa9e69
SangDon <ju00moon@gmail.com>
2025-06-19 20:50:03 +0900

    [feat] BT 수정에 필요한 태스크노드 및 청각감지후 일정시간후 해제되게 정상화

---
+++ LastCanary/Source/LastCanary/AI/Task/BTTask_FacePlayer.cpp
@@ -0,0 +1,50 @@
+﻿#include "AI/Task/BTTask_FacePlayer.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "GameFramework/Pawn.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+UBTTask_FacePlayer::UBTTask_FacePlayer()
+{
+    NodeName = TEXT("Face Player");
+    bNotifyTick = true;  // TickTask를 호출하도록
+}
+
+EBTNodeResult::Type UBTTask_FacePlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+    // 바로 InProgress로 리턴하면 TickTask가 호출됩니다
+    return EBTNodeResult::InProgress;
+}
+
+void UBTTask_FacePlayer::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+    UBlackboardComponent* BB = OwnerComp.GetBlackboardComponent();
+    UObject* Obj = BB->GetValueAsObject(TEXT("TargetActor"));
+    AActor* Target = Cast<AActor>(Obj);
+    AAIController* AICon = OwnerComp.GetAIOwner();
+    APawn* Pawn = AICon ? AICon->GetPawn() : nullptr;
+
+    if (!Target || !Pawn)
+    {
+        FinishLatentTask(OwnerComp, EBTNodeResult::Failed);
+        return;
+    }
+
+    // 1) 방향 계산
+    FVector ToTarget = (Target->GetActorLocation() - Pawn->GetActorLocation()).GetSafeNormal2D();
+    FRotator Desired = ToTarget.Rotation();
+    FRotator Current = Pawn->GetActorRotation();
+    float YawDiff = FMath::FindDeltaAngleDegrees(Current.Yaw, Desired.Yaw);
+
+    // 2) 단계별 회전
+    float MaxStep = RotateSpeed * DeltaSeconds;
+    float YawStep = FMath::Clamp(YawDiff, -MaxStep, +MaxStep);
+    Pawn->SetActorRotation(Current + FRotator(0, YawStep, 0));
+
+    // 3) 완료 조건
+    if (FMath::Abs(YawDiff) <= AcceptanceAngle)
+    {
+        FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+    }
+}
---
+++ LastCanary/Source/LastCanary/AI/Task/BTTask_FollowPlayer.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_FollowPlayer.generated.h"
+
+UCLASS()
+class LASTCANARY_API UBTTask_FollowPlayer : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+    UBTTask_FollowPlayer();
+
+    virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+    /** 플레이어에 근접했을 때 멈출 반경 */
+    UPROPERTY(EditAnywhere, Category = "FollowPlayer")
+    float AcceptanceRadius = 700.f;
+	
+};
